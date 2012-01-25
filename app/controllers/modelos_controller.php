4a7d6bff2ad46247bdb09e452a20ceb5eee5596f
manuel <manuel@dhcppc0.unassigned>
2012-01-25 20:10:12 +0000

    creado controlador modelos, para practicas de validaciones y callbacks

--- app/controllers/modelos_controller.php
+++ app/controllers/modelos_controller.php
@@ -1,5 +1,7 @@
 <?php
 
+Load::models('menu');
+
 class ModelosController extends AppController {
 
     public function index() {
@@ -7,7 +9,16 @@ class ModelosController extends AppController {
     }
     
     public function usos_callback_1(){
-        
+        if ( Input::hasPost('array_formulario') ){
+            $model = new Menu(Input::post('array_formulario'));
+
+            if ( $model->save() ){
+                Flash::valid("El menu $model->nombre fué creado exitosamente...!!!");
+                Input::delete('array_formulario');
+            }else{
+                Flash::error('No se Han podido Guardar los datos...!!!');
+            }
+        }
     }
 
 }
