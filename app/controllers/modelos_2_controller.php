27d6e9006a9ea74e26b4da074fd606784182bf9d
Manuel Aguirre <programador.manuel@gmail.com>
2012-01-28 13:34:03 -0430

    mas cambios :-/

--- app/controllers/modelos_2_controller.php
+++ app/controllers/modelos_2_controller.php
@@ -28,4 +28,33 @@ class Modelos2Controller extends AppController {
          */
         $this->resultado = Load::model('menu')->find();
     }
+
+    public function find_first(){
+        /**
+         * El metodo find_first devulve el primer registro encontrado en la
+         * bd que cumpla con las condiciones pasadas como parametro,
+         * generalmente se le pasa un numero entero que representa el id del
+         * registro que se desea devolver, en el ejemplo no se le está pasando
+         * ningun parametro, por lo tanto el metodo devolverá el primer
+         * registro que consiga en la tabla.
+         */
+        $this->resultado = Load::model('menu')->find_first();
+        /**
+         * Otra manera de llamar al metodo find_first().
+         *
+         * En este ejemplo si se pasa un parametro que es el id del
+         * usuario que se quiere devolver.
+         */
+        Load::models('usuario');
+        $usr = new Usuario();
+        $this->usuario =$usr->find_first(1);
+
+        /**
+         * Ahora si le pasamos una condicion al metodo.
+         * Devolverá el ultimo registro activo que encuentre en la bd
+         */
+        $menu = Load::model('menu');
+        $this->menu = $menu->find_first('activo = 1','order: id DESC');
+
+    }
 }
