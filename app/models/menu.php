061c416bd4891807f5c41e0f7248b67580f41380
Manuel Aguirre <programador.manuel@gmail.com>
2012-01-28 12:59:34 -0430

    cambios en los tutos de modelos_2

--- app/models/menu.php
+++ app/models/menu.php
@@ -4,7 +4,7 @@ class Menu extends ActiveRecord {
 
     //metodo equivalente al constructor de la clase, es llamado en el
     //constructor padre
-    public function initialize(){
+    public function initialize() {
         //por defecto el active record va a validar los campos que sean
         //NOT NULL en la tabla de la base de datos
         //En este caso la tabla menu tiene 2 campos not null:
@@ -12,10 +12,13 @@ class Menu extends ActiveRecord {
         //sin embargo para efectos del ejemplo se hará uso de las validaciones
         //de active record para cambiar los mensajes a mostrar en caso de
         //error en la validacion de los campos.
-        $this->validates_presence_of('nombre','message: Debe Escribir un Nombre para el Menú...!!!');
-        $this->validates_presence_of('url','message: Debe Escribir una URL para el Menú...!!!');
-        $this->validates_uniqueness_of('nombre','message: Este nombre de menú ya existe en el sistema');
+        $this->validates_presence_of('nombre', 'message: Debe Escribir un Nombre para el Menú...!!!');
+        $this->validates_presence_of('url', 'message: Debe Escribir una URL para el Menú...!!!');
+        $this->validates_uniqueness_of('nombre', 'message: Este nombre de menú ya existe en el sistema');
     }
 
-    
+    public function getEstado() {
+        return $this->activo ? 'Activo' : 'Inactivo';
+    }
+
 }
